9e951dea18b28306cb06c44b8cb9b4c3a10e4e5e
pascualperez <pascualpb@hotmail.com>
2016-09-16 12:32:17 +0200

    cordova con bootstrap2

--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,20 @@
 var app = {
-    // Application Constructor
-    initialize: function() {
-        console.log("Inicializando app");
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-        console.log("App inicializada");
-    },
-    onDeviceReady: function() {
-        console.log("Dispositivo preparado");
-    },    
+  // Application Constructor
+  initialize: function() {
+    console.log("Inicializando app");
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+    console.log("App inicializada");
+    $("#btnfoto").click(function(event) {
+      alert("Ahora me haria la foto");
+    });
+  },
+  onDeviceReady: function() {
+    console.log("Dispositivo preparado");
+    $("#btnfoto").click(function(event) {
+      alert("Ahora me haria la foto");
+    });
+
+  },
 };
 
 app.initialize();
