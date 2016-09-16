9752a338a683835229a179fe8d679a50366acb1c
pascualperez <pascualpb@hotmail.com>
2016-09-16 11:47:38 +0200

    cordova con bootstrap

---
+++ www/js/index.js
@@ -0,0 +1,13 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        console.log("Inicializando app");
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        console.log("App inicializada");
+    },
+    onDeviceReady: function() {
+        console.log("Dispositivo preparado");
+    },    
+};
+
+app.initialize();
